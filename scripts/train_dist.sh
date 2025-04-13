98d24d40a34eb1a53b9d2fc4e52aa21d354f11bf
Carter McClellan <carter.s.mcclellan@gmail.com>
2025-04-13 12:56:17 -0700

    distributed training script

---
+++ scripts/train_dist.sh
@@ -0,0 +1,8 @@
+accelerate launch --config_file config/train_dist.yaml train.py \
+--train_meta_file dataset/FIGR-SVG-train.csv \
+--val_meta_file dataset/FIGR-SVG-valid.csv \
+--svg_folder dataset/FIGR-SVG-svgo \
+--output_dir proj_log/ \
+--project_name FIGR_SVG \
+--maxlen 512 \
+--batchsize 20
