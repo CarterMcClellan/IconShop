26e6e9ff0d629e5dea51a53a3d55a109bdba23a5
Carter McClellan <carter.s.mcclellan@gmail.com>
2025-04-13 12:54:53 -0700

    small import error

--- model/layers/transformer.py
+++ model/layers/transformer.py
@@ -9,7 +9,8 @@ from torch.nn.modules.dropout import Dropout
 from torch.nn.modules.linear import Linear
 from torch.nn.modules.normalization import LayerNorm
 
-from .attention import MultiheadAttention
+# from .attention import MultiheadAttention
+from torch.nn import MultiheadAttention
 
 
 class Transformer(Module):
