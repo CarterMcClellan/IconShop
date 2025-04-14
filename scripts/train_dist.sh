eb203e7a0317b00db061bdf7e4be3f89fec066ae
Carter McClellan <carter.s.mcclellan@gmail.com>
2025-04-14 12:09:04 -0700

    time mark runs

--- scripts/train_dist.sh
+++ scripts/train_dist.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+# Get current timestamp in format YYYYMMDD_HHMMSS
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
 accelerate launch --config_file config/train_dist.yaml train.py \
 --train_meta_file dataset/FIGR-SVG-train.csv \
 --val_meta_file dataset/FIGR-SVG-valid.csv \
 --svg_folder dataset/FIGR-SVG-svgo \
 --output_dir proj_log/ \
---project_name FIGR_SVG \
+--project_name FIGR_SVG_${TIMESTAMP} \
 --maxlen 512 \
 --batchsize 20
